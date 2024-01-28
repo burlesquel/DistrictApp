9dfc719d81495dd30e5bc7c1c0d7a15e9de2c4ed
Batuhan Manav <batumanav@gmail.com>
2024-01-28 06:34:59 +0300

    other profiles photourl

--- pages/app/profile/[id].vue
+++ pages/app/profile/[id].vue
@@ -39,8 +39,8 @@ const { pending, data, error } = await useAsyncData(`user_${user_id}`, async () 
                     </div>
                     <div class="mb-5">
                         <div class="flex flex-col items-center justify-center">
-                            <img :src="store.user.photoURL" alt="" class="mb-5 h-24 w-24 rounded-full object-cover" />
-                            <p class="text-xl font-semibold text-primary">{{ data?.displayName }}</p>
+                            <img :src="data.photoURL" alt="" class="mb-5 h-24 w-24 rounded-full object-cover" />
+                            <p class="text-xl font-semibold text-primary">{{ data.displayName }}</p>
                         </div>
                         <ul class="m-auto mt-5 flex max-w-[160px] flex-col space-y-4 font-semibold text-white-dark">
                             <li class="flex items-center gap-2">
